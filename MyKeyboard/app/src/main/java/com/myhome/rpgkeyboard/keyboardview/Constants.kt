eb0af75556a5db972df7d38ee67c5833a94b4dd6
HojoonKim <hojoon905@naver.com>
2024-08-17 21:48:47 +0900

    UPDATE: replaceMap

--- MyKeyboard/app/src/main/java/com/myhome/rpgkeyboard/keyboardview/Constants.kt
+++ MyKeyboard/app/src/main/java/com/myhome/rpgkeyboard/keyboardview/Constants.kt
@@ -4,5 +4,8 @@ const val SERVER_IP: String = "192.168.174.202" // 서버 IP 주소
 const val SERVER_PORT: Int = 12345 // 서버 포트 번호
 
 val replacementMap = mapOf(
-    "하트" to "♥"
+    "하트" to "♥",
+    "ㅅㅂ" to "**",
+    "ㅂㅅ" to "^^",
+    "ㅅ ㅂ" to "**"
 )
